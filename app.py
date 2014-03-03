33ed1efdbe5115d91a91b38d4c5893ebae386568
sprt <hellosprt@gmail.com>
2014-03-03 14:21:50 +0100

    Erase cache on version bump

--- app.py
+++ app.py
@@ -7,7 +7,11 @@ IS_DEV = os.environ['SERVER_SOFTWARE'].startswith('Development')
 @get('/')
 @view('home')
 def get_home():
-    return {'IS_DEV': IS_DEV}
+    app_major_version = os.environ['CURRENT_VERSION_ID'].split('.')[0]
+    return {
+        'IS_DEV': IS_DEV,
+        'js_suffix': '.js' if IS_DEV else '.min.js?' + app_major_version
+        }
 
 @get('/_ah/warmup')
 def get_warmup():
